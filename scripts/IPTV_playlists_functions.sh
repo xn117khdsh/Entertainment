b0172f5b47de07bd2048610cb0cf4ba371740e3f
xn117khdsh <62739811+xn117khdsh@users.noreply.github.com>
2025-08-28 19:05:57 +0700

    Update IPTV_playlists_functions.sh

--- scripts/IPTV_playlists_functions.sh
+++ scripts/IPTV_playlists_functions.sh
@@ -4,9 +4,6 @@ user_agent="Dalvik/2.1.0 (Linux; U; Android 10; xn117khdsh/Entertainment-Github_
 real_user_agent="Dalvik/2.1.0 (Linux; U; Android 10; Redmi K20 Build/SKQ1.211001.060)"
 
 function sn() {
-    wget -qO- -U "$user_agent" 
-    "http://sharing.gotdns.ch:8091/cakhia.php" 
-    "http://sharing.gotdns.ch:8091/socolive.php" 
-    "http://sharing.gotdns.ch:8091/luongsontv.php" > ./playlists/sn-thethao.m3u
+   wget -i "https://freem3u.xyz/r/smavruuhg" -U "$user_agent" -O ./playlists/sn-thethao.m3u
 
 }
