bb2d7e97d225766c6286c819cdf30cc3bfcde9b4
xn117khdsh <62739811+xn117khdsh@users.noreply.github.com>
2026-02-07 01:52:06 +0700

    Update IPTV_playlists_functions.sh

--- scripts/IPTV_playlists_functions.sh
+++ scripts/IPTV_playlists_functions.sh
@@ -4,7 +4,8 @@ user_agent="Dalvik/2.1.0 (Linux; U; Android 10; xn117khdsh/Entertainment-Github_
 real_user_agent="Dalvik/2.1.0 (Linux; U; Android 10; Redmi K20 Build/SKQ1.211001.060)"
 
 function sn() {
-   wget -i "https://freem3u.xyz/r/smavruuhg" -U "$user_agent" -O ./playlists/sn-thethao.m3u
+   # wget -i "https://freem3u.xyz/r/smavruuhg" -U "$user_agent" -O ./playlists/sn-thethao.m3u
+   wget "https://freem3u.xyz/r/wedsjrhdr" -U "OTT Navigator/1.6.9.4 (Linux;Android 12; en; 10i5hoq)" -O ./playlists/sn-thethao.m3u
 }
 
 function simple() {
