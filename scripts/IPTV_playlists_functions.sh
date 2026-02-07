c25dd1a8065f6e22b157863748bdaed394d1899a
xn117khdsh <62739811+xn117khdsh@users.noreply.github.com>
2026-02-08 02:31:45 +0700

    Update IPTV_playlists_functions.sh

--- scripts/IPTV_playlists_functions.sh
+++ scripts/IPTV_playlists_functions.sh
@@ -11,3 +11,7 @@ function sn() {
 function simple() {
     wget "https://freem3u.xyz/r/cmmgjhdr" -U "OTT Navigator/1.6.9.4 (Linux;Android 12; en; 10i5hoq)" -O ./playlists/simple.m3u
 }
+
+function onetv() {
+    wget "https://freem3u.xyz/r/dpdyehvg" -U "$user_agent" -O ./playlists/onetv.m3u
+}
